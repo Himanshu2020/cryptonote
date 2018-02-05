ddda60fe848b64b19f5dbd64d4bc23bfd8c0b111
Navin Rakhonde <navin.rakhonde@techaroha.com>
2018-02-05 14:25:36 +0530

    msg

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,8 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "kubercoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char GENESIS_COINBASE_TX_HEX[]                         = "013c01ff0001f5ef0b029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121014d52da27beef3e3f4e3a9f6b2d986db06667cc89003b7b646c07c822009af638";
+
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
